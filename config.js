76bfd9512c1636e6ec137bc89e7158e110be7882
Hiyu-coffee <58n24vh7d4@privaterelay.appleid.com>
2026-09-22 23:47:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 請填入你的 Supabase 專案資訊
 window.HIYU_SUPABASE_URL = "https://buetzexoukixmxeqndlc.supabase.co";
-window.HIYU_SUPABASE_ANON_KEY = "請貼上你的 Supabase Publishable Key";
+window.HIYU_SUPABASE_ANON_KEY = "sb_publishable_SrGLrobKz85B3teCDWHhfw_zXaY_1yR";
